5dca5a6678dbcee239e4206d1b17fa1b90d17973
gasan <abduragimov.ga@gmail.com>
2017-12-26 13:09:52 +0500

    3

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 def app(environ, start_response):
     query_strings = environ['QUERY_STRING']
-    data = str(query_strings).encode()
+    # data = str(query_strings).encode()
     data2 = [bytes(i + '\n', 'ascii') for i in query_strings.split('&')]
     # for query_string in query_strings:
     #     data2 += (query_string + '\n').encode()
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
+        ("Content-Length", str(len(data2)))
     ])
-    return [data]
+    return [data2]
