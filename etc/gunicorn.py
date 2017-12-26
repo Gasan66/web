21490511a666df480e1b30cd5ff022e6e4268711
gasan <abduragimov.ga@gmail.com>
2017-12-26 12:59:57 +0500

    2

---
+++ etc/gunicorn.py
@@ -0,0 +1,7 @@
+bind = '0.0.0.0:8000'
+working_dir = 'home/box/web'
+workers = 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 def app(environ, start_response):
-    data = 'Hello, World!\n'
+    query_strings = environ['QUERY_STRING']
+    data = str(query_strings).encode()
+    data2 = [bytes(i + '\n', 'ascii') for i in query_strings.split('&')]
+    # for query_string in query_strings:
+    #     data2 += (query_string + '\n').encode()
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data])
+    return [data]
