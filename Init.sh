907f9c54ff0486844fa9c1e0de1e675180f13573
gasan <abduragimov.ga@gmail.com>
2017-12-27 15:25:57 +0500

    23

--- Init.sh
+++ Init.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
