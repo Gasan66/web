ac56f9d6eae8b7228213d0509119457fed73bab3
gasan <abduragimov.ga@gmail.com>
2018-01-18 16:41:49 +0500

    4

--- Init.sh
+++ Init.sh
@@ -2,3 +2,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/mysql start
+mysql -u root -e "create database ask"
